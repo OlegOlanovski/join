39420340065d2d189f4cce2489100617978e1d4f
Maik Pankow <maikpankow@gmail.com>
2026-01-31 12:35:44 +0100

    add new function and totals Todos / and Done todos

--- javascript/summary.js
+++ javascript/summary.js
@@ -8,3 +8,34 @@ if (hour < 12) {
 } else {
   greeting.textContent = "Good evening!";
 }
+
+function init() {
+  getTasksTotal();
+  getTasksDone(); 
+}
+
+function getTasksTotal() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  let total_tasks = document.getElementById("todos-total");
+  total_tasks.innerText = tasks.length;
+}
+
+function getTasksDone() {
+  let done_tasks = document.getElementById("todos-done");
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  let Todos_Done = []
+  let status = tasks.status = "done";
+  if (status) {
+    Todos_Done.push(status);
+  }
+  done_tasks.innerText = Todos_Done.length;
+}
+
+
+// function getTasksUrgent() {
+//   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  
+//   let done_tasks = document.getElementById("todos total");
+//   done_tasks.innerText = tasks.length;
+//   console.log(tasks.length);
+// }
