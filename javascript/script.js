b75f12de5091dad8b527d37ed467a36a61bc7a92
Maik Pankow <maikpankow@gmail.com>
2026-02-09 17:56:31 +0100

    add debug function script js add script tag for add task

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+
+// async function logIn() {
+//   const db = "https://join-da53b-default-rtdb.firebaseio.com/";
+
+//   const response = await fetch(db + ".json");
+//   const loginData = await response.json();
+
+//   console.log("RAW Firebase Data:", loginData);
+// }
